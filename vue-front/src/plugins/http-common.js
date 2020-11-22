af84c9338a0eb4c8a5ca193a1037179941c252d8
Florian Burel <fl0@mac.com>
2020-11-22 01:12:49 +0100

    add prod url

--- vue-front/src/plugins/http-common.js
+++ vue-front/src/plugins/http-common.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "http://localhost:3000",
+    baseURL: process.env.NODE_ENV === 'development'
+    ? 'http://localhost:3000'
+    : 'http://ec2-15-188-147-85.eu-west-3.compute.amazonaws.com',
     headers: {
         "Content-type": "application/json"
     }
